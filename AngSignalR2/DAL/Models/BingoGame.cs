1ff944dbade743973297066d44cce63babda6b0a
Kyle Ekstrand <KSEkstrand@gmail.com>
2015-05-29 00:23:52 -0800

    game constructor
    Finisehd constructor for a game? maybe. Too sleepy to look at random
    loop right now.
    
    Deleted bingo ball because it's just ints i mean cmon.

--- AngSignalR2/DAL/Models/BingoGame.cs
+++ AngSignalR2/DAL/Models/BingoGame.cs
@@ -9,24 +9,47 @@ namespace AngSignalR2.DAL.Models
     {
         public int Id { get; set; }
         public ICollection<BingoUser> BingoUsers { get; set; }
-        public ICollection<BingoBall> UsedBingoBalls { get; set; }
-        public ICollection<BingoBall> FreeBingoBalls { get; set; }
+        public int[] UsedBingoBalls { get; set; }
+        public int[] FreeBingoBalls { get; set; }
         public ICollection<Message> Messages { get; set; }
         public string BingoWinnerUsername { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime? EndTime { get; set; }
 
-        //public BingoGame(BingoUser[] Users)
-        //{
-        //    BingoUsers = Users;
-        //}
+        public BingoGame(BingoUser[] Users)
+        {
+            
+            UsedBingoBalls = new int[75];
+            FreeBingoBalls = new int[75];
+            BingoUsers = Users;
+            StartTime = DateTime.Now;
+            Random _r = new Random();
+            for (int i = 0; i < FreeBingoBalls.Length; i++)
+            {
+                FreeBingoBalls[i] = Rand(_r);
+            }
+            
+        }
+
+        private int Rand(Random _r)
+        {            
+            int num = 0;
+            
+            num = _r.Next(1, 75);
+            if (!FreeBingoBalls.Contains(num))
+            {
+                return num;
+            }
+            else
+                return Rand(_r);
+        }
     }
 
     public class BingoGameVM
     {
         public int Id { get; set; }
         public List<string> BingoGameUsernames { get; set; }
-        public IEnumerable<int> UsedBingoBalls { get; set; }
-        public IEnumerable<int> FreeBingoBalls { get; set; }
+        public int[] UsedBingoBalls { get; set; }
+        public int[] FreeBingoBalls { get; set; }
     }
 }
